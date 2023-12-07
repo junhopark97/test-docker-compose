2c2018231c87c826051643d89426eb96d642a0cb
junhopark97 <jh97.dev@gmail.com>
2023-12-07 14:16:34 +0900

    2

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def index() -> str:
     print(requests.get("http://localhost:8000/securities").json())
     # response = requests.get(f"{ISSUANCE_SEVER_URI}" + "/securities")
     # return response.json()
-    # rebase
+    # rebase 1
     return 'Hello World'
 
 
